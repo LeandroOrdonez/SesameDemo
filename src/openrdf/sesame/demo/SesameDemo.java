4610880cd799745056d12e1a7104dde6c37c5c24
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-20 14:50:22 +0200

    Update: method addStatementTest created.

--- src/openrdf/sesame/demo/SesameDemo.java
+++ src/openrdf/sesame/demo/SesameDemo.java
@@ -7,7 +7,14 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openrdf.OpenRDFException;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.GraphQuery;
 import org.openrdf.query.GraphQueryResult;
@@ -95,6 +102,60 @@ public class SesameDemo {
         }
     }
 
+    public static void addStatementTest(Repository repo) {
+        try {
+            ValueFactory factory = repo.getValueFactory();
+            //create some resources and literals to make statements out of them
+            //-- Classes
+            URI documentClass = factory.createURI("http://www.example.org/category-structure.rdf#Document");
+            URI categoryClass = factory.createURI("http://www.example.org/category-structure.rdf#Category");
+            URI membershipRelClass = factory.createURI("http://www.example.org/category-structure.rdf#Membership_Relation");
+            URI termRelClass = factory.createURI("http://www.example.org/category-structure.rdf#Term_Relation");
+            URI termClass = factory.createURI("http://www.example.org/category-structure.rdf#Term");
+
+            //-- Instances
+            URI document1 = factory.createURI("http://www.example.org/documents/1");
+            URI category3 = factory.createURI("http://www.example.org/categories/3");
+            URI membershipRel3 = factory.createURI("http://www.example.org/membership_relations/3");
+            URI termRel3 = factory.createURI("http://www.example.org/term_relations/3");
+            URI term1 = factory.createURI("http://www.example.org/terms/1");
+
+            //-- Properties
+            URI is_member_of = factory.createURI("http://www.example.org/category-structure.rdf#is_member_of");
+            URI category_value = factory.createURI("http://www.example.org/category-structure.rdf#category_value");
+            URI membership_probability = factory.createURI("http://www.example.org/category-structure.rdf#membership_probability");
+            URI has_term = factory.createURI("http://www.example.org/category-structure.rdf#has_term");
+            URI term_value = factory.createURI("http://www.example.org/category-structure.rdf#term_value");
+            URI term_probability = factory.createURI("http://www.example.org/category-structure.rdf#term_probability");
+            URI has_content = factory.createURI("http://www.example.org/category-structure.rdf#has_content");
+
+            //-- Literals
+            //Literal membership_probability_literal = factory.createLiteral(0.7);
+            //Literal term_probability_literal = factory.createLiteral(0.4);
+            
+            RepositoryConnection con = repo.getConnection();
+            try{
+                con.add(document1, RDF.TYPE, documentClass);
+                con.add(document1, is_member_of, membershipRel3);
+                con.add(category3, RDF.TYPE, categoryClass);
+                con.add(category3, has_term, termRel3);
+                con.add(membershipRel3, RDF.TYPE, membershipRelClass);
+                con.add(membershipRel3, category_value, category3);
+                con.add(membershipRel3, membership_probability, factory.createLiteral(0.55));
+                con.add(termRel3, RDF.TYPE, termRelClass);
+                con.add(termRel3, term_value, term1);
+                con.add(termRel3, term_probability, factory.createLiteral(0.7));                
+                con.add(term1, RDF.TYPE, termClass);
+                con.add(term1, has_content, factory.createLiteral("HolaMundo!!!"));
+                
+            } finally {
+                con.close();
+            }
+        } catch (RepositoryException ex) {
+            Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) {
         try {
             String sesameServer = "http://localhost:8080/openrdf-sesame/";
@@ -119,6 +180,9 @@ public class SesameDemo {
             FileOutputStream out2 = new FileOutputStream("src/outcome/graph-query-result.srx");
             SesameDemo.issueGraphQuery(repo, queryString2, out2);
             
+            //--------------- Adding some statements ----------------------
+            addStatementTest(repo);
+
             //Shutdown the repository when all the operations have been done
             repo.shutDown();
 
