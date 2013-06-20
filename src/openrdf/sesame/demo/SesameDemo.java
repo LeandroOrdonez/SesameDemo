0b513749814ffe414f21bff13dda2f68a1de33d8
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-20 11:33:30 +0200

    Retrieving tuples from a Sesame Repository (via HTTPRepository)

---
+++ src/openrdf/sesame/demo/SesameDemo.java
@@ -0,0 +1,70 @@
+package openrdf.sesame.demo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openrdf.OpenRDFException;
+import org.openrdf.model.Value;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+
+/**
+ *
+ * @author Leandro Ordonez <leandro.ordone.ante@gmail.com>
+ */
+public class SesameDemo {
+
+    public static void main(String[] args) {
+        try {
+            String sesameServer = "http://localhost:8080/openrdf-sesame/";
+            String repositoryID = "WebAPIModel";
+            Repository repo = new HTTPRepository(sesameServer, repositoryID);
+            repo.initialize();
+            System.out.println("Ready!");
+            try {
+                RepositoryConnection con = repo.getConnection();
+                try {
+                    String queryString = "SELECT ?x ?p ?y WHERE { ?x ?p ?y } ";
+                    TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+
+                    TupleQueryResult result = tupleQuery.evaluate();
+                    try {
+                        while (result.hasNext()) {
+                            BindingSet bindingSet = result.next();
+                            Value valueOfX = bindingSet.getValue("x");
+                            Value valueOfP = bindingSet.getValue("p");
+                            Value valueOfY = bindingSet.getValue("y");
+                            // do something interesting with the values here...
+                            System.out.println("?x: " + valueOfX.stringValue());
+                            System.out.println("?p: " + valueOfP.stringValue());
+                            System.out.println("?y: " + valueOfY.stringValue());
+                        }
+                    } catch (Exception ex) {
+                        Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+                    } finally {
+                        result.close();
+                    }
+                } catch (MalformedQueryException ex) {
+                    Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (QueryEvaluationException ex) {
+                    Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    con.close();
+                }
+            } catch (OpenRDFException e) {
+                // handle exception
+            }
+        } catch (RepositoryException ex) {
+            Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            System.exit(0);
+        }
+    }
+}
