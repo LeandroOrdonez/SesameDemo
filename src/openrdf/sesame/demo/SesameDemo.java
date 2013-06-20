38cfcfd30d36838b25f824b828d0d14b5d326a88
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-20 11:45:03 +0200

    Update: method issueTupleQuery() created

--- src/openrdf/sesame/demo/SesameDemo.java
+++ src/openrdf/sesame/demo/SesameDemo.java
@@ -20,18 +20,11 @@ import org.openrdf.repository.http.HTTPRepository;
  * @author Leandro Ordonez <leandro.ordone.ante@gmail.com>
  */
 public class SesameDemo {
-
-    public static void main(String[] args) {
+    
+    public static void issueTupleQuery(Repository repo, String queryString) {
         try {
-            String sesameServer = "http://localhost:8080/openrdf-sesame/";
-            String repositoryID = "WebAPIModel";
-            Repository repo = new HTTPRepository(sesameServer, repositoryID);
-            repo.initialize();
-            System.out.println("Ready!");
-            try {
                 RepositoryConnection con = repo.getConnection();
                 try {
-                    String queryString = "SELECT ?x ?p ?y WHERE { ?x ?p ?y } ";
                     TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
 
                     TupleQueryResult result = tupleQuery.evaluate();
@@ -45,6 +38,7 @@ public class SesameDemo {
                             System.out.println("?x: " + valueOfX.stringValue());
                             System.out.println("?p: " + valueOfP.stringValue());
                             System.out.println("?y: " + valueOfY.stringValue());
+                            //-------------------------------------------------
                         }
                     } catch (Exception ex) {
                         Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
@@ -61,6 +55,17 @@ public class SesameDemo {
             } catch (OpenRDFException e) {
                 // handle exception
             }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String sesameServer = "http://localhost:8080/openrdf-sesame/";
+            String repositoryID = "WebAPIModel";
+            Repository repo = new HTTPRepository(sesameServer, repositoryID);
+            repo.initialize();
+            System.out.println("Ready!");
+            String queryString = "SELECT ?x ?p ?y WHERE { ?x ?p ?y } ";
+            SesameDemo.issueTupleQuery(repo, queryString);
         } catch (RepositoryException ex) {
             Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
