bf943aabc028393e736130126097f12d13f0394f
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-20 13:12:32 +0200

    Update: method issueTupleQuery(). Generates a .srx file with the query results when a FileOutputStream object is passed as parameter.

--- src/openrdf/sesame/demo/SesameDemo.java
+++ src/openrdf/sesame/demo/SesameDemo.java
@@ -1,5 +1,8 @@
 package openrdf.sesame.demo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,6 +14,7 @@ import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLWriter;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -21,13 +25,16 @@ import org.openrdf.repository.http.HTTPRepository;
  * @author Leandro Ordonez <leandro.ordone.ante@gmail.com>
  */
 public class SesameDemo {
-    
-    public static void issueTupleQuery(Repository repo, String queryString) {
-        try {
-                RepositoryConnection con = repo.getConnection();
-                try {
-                    TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
 
+    public static void issueTupleQuery(Repository repo, String queryString, FileOutputStream fileOut) {
+        try {
+            RepositoryConnection con = repo.getConnection();
+            try {
+                TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+                if (fileOut != null) {
+                    SPARQLResultsXMLWriter sparklWriter = new SPARQLResultsXMLWriter(fileOut);
+                    tupleQuery.evaluate(sparklWriter);
+                } else {
                     TupleQueryResult result = tupleQuery.evaluate();
                     try {
                         List<String> bindingNames = result.getBindingNames();
@@ -47,16 +54,17 @@ public class SesameDemo {
                     } finally {
                         result.close();
                     }
-                } catch (MalformedQueryException ex) {
-                    Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (QueryEvaluationException ex) {
-                    Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
-                } finally {
-                    con.close();
                 }
-            } catch (OpenRDFException e) {
-                // handle exception
+            } catch (MalformedQueryException ex) {
+                Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (QueryEvaluationException ex) {
+                Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                con.close();
             }
+        } catch (OpenRDFException e) {
+            // handle exception
+        }
     }
 
     public static void main(String[] args) {
@@ -67,8 +75,14 @@ public class SesameDemo {
             repo.initialize();
             System.out.println("Ready!");
             String queryString = "SELECT ?x ?p ?y WHERE { ?x ?p ?y } ";
-            SesameDemo.issueTupleQuery(repo, queryString);
+            //Print the results from issuing the query in System console.
+            SesameDemo.issueTupleQuery(repo, queryString, null);
+            //Save the results from issuing the query into a file.
+            FileOutputStream out = new FileOutputStream("src/outcome/result.srx");
+            SesameDemo.issueTupleQuery(repo, queryString, out);
         } catch (RepositoryException ex) {
+            Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(SesameDemo.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             System.exit(0);
